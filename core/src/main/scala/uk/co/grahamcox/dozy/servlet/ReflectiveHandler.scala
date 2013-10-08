37974a53ef0a89bc56701f56b7e3c5c60cf54376
Graham Cox <graham@grahamcox.co.uk>
2013-10-08 23:42:28 +0100

    Enough to be able to reflectively call a method on a bean as long as there are no arguments to the method. Includes crude handling of exceptions and response mappings

--- core/src/main/scala/uk/co/grahamcox/dozy/servlet/ReflectiveHandler.scala
+++ core/src/main/scala/uk/co/grahamcox/dozy/servlet/ReflectiveHandler.scala
@@ -3,20 +3,52 @@ package uk.co.grahamcox.dozy.servlet
 import grizzled.slf4j.Logger
 import java.lang.annotation.Annotation
 import java.lang.reflect.Method
+import java.lang.reflect.InvocationTargetException
 import uk.co.grahamcox.dozy.annotations._
+import uk.co.grahamcox.dozy.annotations.{Method => HttpMethod}
 import uk.co.grahamcox.dozy.annotations.{Handler => HandlerMethod}
 
 /**
  * Handler that works using Java Reflection to wrap a bean and Method
  */
 class ReflectiveHandler(bean: Any, method: Method) extends Handler {
+  /** The logger to use */
+  private val logger = Logger[this.type]
+  
+  /** The HTTP Method to use */
+  private val httpMethod = getAnnotation[HttpMethod] map { a => a.value } getOrElse("GET")
+  /** The HTTP Path to use */
+  private val httpPath = new PathMatcher(getAnnotations[Path] map { a => a.value } map { a => a.stripPrefix("/") } mkString("/", "/", ""))
+
+  logger.debug(s"Method: $httpMethod, Path: $httpPath")
   /**
    * Handle the request provided
    * @param req The request to handle
    * @return the response from handling the request
    */
   def handle(req: Request): Response = {
-    Response(200)
+    val args = buildParameters(req)
+    try {
+      val output = try {
+        method.invoke(bean, args : _*)
+      }
+      catch {
+        case e: InvocationTargetException => throw e.getCause()
+        case e: Exception => throw e
+      }
+
+      logger.debug(s"Method $method responded with $output")
+      output match {
+        case r: Response => r
+        case r: Any => Response(payload = Some(r))
+      }
+    }
+    catch {
+      case e: Exception => {
+        logger.error("Unhandled exception", e)
+        throw e;
+      }
+    }
   }
 
   /**
@@ -25,7 +57,49 @@ class ReflectiveHandler(bean: Any, method: Method) extends Handler {
    * @return True if we can handle the request. False if not
    */
   def canHandle(req: Request): Boolean = {
-    false
+    req.getMethod.eq(httpMethod) && httpPath.matchPath(req.getURL).matches
+  }
+
+  /**
+   * Build the array of parameters needed to execute the wrapped method
+   * @param req The request to build the parameters from
+   * @return the array of parameters
+   */
+  private def buildParameters(req: Request): Array[_ <: Object] = Nil.toArray
+
+  /**
+   * Helper to get the requested annotation from the method we are working on
+   * @param manifest The type of annotation to get
+   * @return the annotation
+   */
+  private def getAnnotationFromMethod[T <: Annotation](implicit manifest: Manifest[T]): Option[T] = {
+    Option(method.getAnnotation(manifest.runtimeClass.asInstanceOf[Class[Annotation]])).map { a => a.asInstanceOf[T] }
+  }
+
+  /**
+   * Helper to get the requested annotation from the class of the method we are working on
+   * @param manifest The type of annotation to get
+   * @return the annotation
+   */
+  private def getAnnotationFromClass[T <: Annotation](implicit manifest: Manifest[T]): Option[T] = {
+    Option(bean.getClass.getAnnotation(manifest.runtimeClass.asInstanceOf[Class[Annotation]])).map { a => a.asInstanceOf[T] }
+  }
+
+  /**
+   * Get the annotation of the given type from either the method or the class as needed
+   * @param manifest The type of annotation to get
+   * @return the annotation
+   */
+  private def getAnnotation[T <: Annotation](implicit manifest: Manifest[T]): Option[T] = {
+    getAnnotationFromMethod[T].orElse(getAnnotationFromClass[T])
+  }
+  /**
+   * Get the annotations of the given type from both the class and the method, in that order
+   * @param manifest The type of annotation to get
+   * @return the annotations
+   */
+  private def getAnnotations[T <: Annotation](implicit manifest: Manifest[T]): Traversable[T] = {
+    Seq(getAnnotationFromClass[T], getAnnotationFromMethod[T]).flatten
   }
 }
 
--- core/src/test/scala/uk/co/grahamcox/dozy/servlet/ReflectiveHandlerSpec.scala
+++ core/src/test/scala/uk/co/grahamcox/dozy/servlet/ReflectiveHandlerSpec.scala
@@ -8,6 +8,46 @@ import uk.co.grahamcox.dozy.annotations._
 import uk.co.grahamcox.dozy.annotations.{Handler => HandlerMethod}
 
 class ReflectiveHandlerSpec extends Specification with Mockito {
+  "The handler" should {
+    "when executing a no-args method" in {
+      abstract class Handler {
+        def handle(): Any
+      }
+      val method = classOf[Handler].getMethod("handle")
+      val request = mock[Request]
+
+      "that returns a Response object" in {
+        val handler = mock[Handler]
+        val reflectiveHandler = new ReflectiveHandler(handler, method)
+        handler.handle() returns Response(300)
+        val response = reflectiveHandler.handle(request)
+        response must beAnInstanceOf[Response]
+        response.statusCode must beEqualTo(300)
+        response.payload must beNone
+        there was one(handler).handle()
+      }
+
+      "that returns a String object" in {
+        val handler = mock[Handler]
+        val reflectiveHandler = new ReflectiveHandler(handler, method)
+        handler.handle() returns "Hello, World"
+        val response = reflectiveHandler.handle(request)
+        response must beAnInstanceOf[Response]
+        response.statusCode must beEqualTo(200)
+        response.payload must beSome("Hello, World")
+        there was one(handler).handle()
+      }
+
+      "that throws" in {
+        val handler = mock[Handler]
+        val reflectiveHandler = new ReflectiveHandler(handler, method)
+        handler.handle() throws new NullPointerException
+        reflectiveHandler.handle(request) must throwA[NullPointerException]
+        there was one(handler).handle()
+      }
+    }
+  }
+
   "The builder" should {
     "find no methods on String" in {
       val handlers = ReflectiveHandler.build("hello")
